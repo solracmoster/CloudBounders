3e53e7bf186cfd726c88e2da91f3370a85e08bc5
Carlos Magana <18.carlos.magana@ksd.org>
2018-05-09 10:02:08 -0700

    Title stuff

---
+++ Cloud Bounders/Assets/Scripts/Quit.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Quit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+		void Update() {
+			if (Input.GetKey("escape"))
+				Application.Quit();
+
+		}
+	}
