2d537b377193eb649cdc7ec4bf8363503fd86434
Carlos Magana <18.carlos.magana@ksd.org>
2018-05-14 10:02:09 -0700

    jumping

---
+++ Cloud Bounders/Assets/Scripts/Jumpa.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumpa : MonoBehaviour
+{
+
+    [Range(1, 20)]
+    public float jumpVelocity;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.up * jumpVelocity;
+        }
+    }
+}
